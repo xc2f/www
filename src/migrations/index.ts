8c06b128c24cf13456748731fecc2a670ab625c5
Fen <hellowd93@163.com>
2026-01-07 03:08:57 +0800

    update readme

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20251229_delete_feeds from './20251229_delete_feeds';
-import * as migration_20260106_184630 from './20260106_184630';
+import * as migration_20260106_184630 from './20260106_184630'
 
 export const migrations = [
   {
-    up: migration_20251229_delete_feeds.up,
-    down: migration_20251229_delete_feeds.down,
-    name: '20251229_delete_feeds',
-  },
-  {
     up: migration_20260106_184630.up,
     down: migration_20260106_184630.down,
-    name: '20260106_184630'
+    name: '20260106_184630',
   },
-];
+]
